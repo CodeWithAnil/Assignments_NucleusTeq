06c20beaf5c1144c5e9f4e9594e05dae58accdc2
anil vishwakarma <anilvishwakarma4494@gmail.com>
2024-06-08 14:04:27 +0530

    first commit

---
+++ capstone_inventory_management_system/app.py
@@ -0,0 +1,67 @@
+from logging.handlers import RotatingFileHandler
+from flask import Flask
+import logging
+from flask_sqlalchemy import SQLAlchemy
+from flask_mysqldb import MySQL
+
+
+# Initialize extensions
+db = SQLAlchemy()
+mysql = MySQL()
+
+def create_app(config_name='default'):
+    app = Flask(__name__)
+    
+    # setUp general logigng 
+    general_log_handler = RotatingFileHandler('logs/general.log')
+    general_log_handler.setLevel(logging.INFO)
+    general_formatter = logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+    )
+    general_log_handler.setFormatter(general_formatter)
+
+    # # Set up error logging
+    # error_log_handler = RotatingFileHandler('logs/error.log', maxBytes=10000, backupCount=3)
+    # error_log_handler.setLevel(logging.ERROR)
+    # error_formatter = logging.Formatter(
+    #     '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+    # )
+    # error_log_handler.setFormatter(error_formatter)
+
+    # Add handlers to the app logger
+    app.logger.addHandler(general_log_handler)
+    # app.logger.addHandler(error_log_handler)
+    app.logger.setLevel(logging.INFO)
+
+
+    if config_name == 'default':
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:anil%40123@localhost/inventory'
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SECRET_KEY'] = 'anil'
+    elif config_name == 'testing':
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:anil%40123@localhost/unittest_inventory'
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SECRET_KEY'] = 'anil'
+        app.config['TESTING'] = True
+        
+    # Initialize sqlalchemy db extensions with the flask app
+    db.init_app(app)
+    if config_name == 'default':
+        mysql.init_app(app)
+
+    # Import blueprints and register them
+    from auth.routes import auth_bp
+
+
+    app.register_blueprint(auth_bp)
+
+
+    # Create database tables if they don't exist
+    with app.app_context():
+        db.create_all()
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
